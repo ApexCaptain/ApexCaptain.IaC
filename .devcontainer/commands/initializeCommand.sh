157c2b5923f51ccf00f23519d7f823cf8f7b6d8b
SangHun Lee <ayteneve93@gmail.com>
2023-09-30 01:24:34 +0900

    init: devcontainer

---
+++ .devcontainer/commands/initializeCommand.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# DevContainer Arguments
+localWorkspaceFolder=$1  
+containerWorkspaceFolder=$2 
+localWorkspaceFolderBasename=$3   
+containerWorkspaceFolderBasename=$4   
+devcontainerId=$5
+
+# Create env for docker-compose 
+cat > .devcontainer/.env <<EOL
+
+# Original DevContainer Arguments
+localWorkspaceFolder = $1 
+containerWorkspaceFolder = $containerWorkspaceFolder
+localWorkspaceFolderBasename =  $localWorkspaceFolderBasename
+containerWorkspaceFolderBasename = $containerWorkspaceFolderBasename 
+devcontainerId = $devcontainerId
+
+# Workspace
+## Anonymous Volume Arguments
+devContainerTerraformOutputDirHostPath = /devContainer/${localWorkspaceFolderBasename}/tfOutputs
+devContainerTerraformOutputDirContainerPath = ${containerWorkspaceFolder}/generated
+
+# Named Volume Arguments
+nodeModulesVolumeContainerPath = ${containerWorkspaceFolder}/node_modules
+EOL
