c489477d54bc2b48d6dfd74db32fc73de12ca027
SangHun Lee <ayteneve93@gmail.com>
2024-10-02 19:01:33 +0900

    RegCommit 2024-10-02

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,8 +1,140 @@
-import { typescript } from "projen";
+import path from 'path';
+import { javascript, typescript } from 'projen';
+import { GithubCredentials } from 'projen/lib/github';
+import { ArrowParens } from 'projen/lib/javascript';
+
+const constants = (() => {
+  const project = {
+    name: 'apex-captain.iac',
+  };
+
+  const author = {
+    name: 'ApexCaptain',
+    email: 'ayteneve93@gmail.com',
+  };
+
+  const branches = {
+    main: 'main',
+    develop: 'develop',
+  };
+
+  const srcDir = 'src';
+  const cdktfOutFilePath = 'cdktf.out.json';
+
+  const paths = {
+    dirs: [srcDir],
+    etc: {
+      cdktfOutFilePath,
+      generatedSrcDir: path.join(srcDir, 'generated'),
+      envDir: 'env',
+    },
+  };
+
+  // @ToDo 추후 재설정
+  const projenCredentials = {
+    githubTokenCredential: GithubCredentials.fromPersonalAccessToken({
+      secret: 'WORKFLOW_TOKEN',
+    }),
+  };
+
+  return {
+    project,
+    author,
+    branches,
+    dirs: paths,
+    projenCredentials,
+  };
+})();
 
 const project = new typescript.TypeScriptAppProject({
-  defaultReleaseBranch: "main",
-  name: "ApexCaptain.IaC",
+  // TypeScript Project Options
+  eslintOptions: {
+    tsconfigPath: './tsconfig.dev.json',
+    dirs: constants.dirs.dirs,
+    ignorePatterns: [
+      '/**/node_modules/*',
+      `${constants.dirs.etc.generatedSrcDir}/`,
+    ],
+    prettier: true,
+  },
   projenrcTs: true,
+  tsconfig: {
+    compilerOptions: {
+      declaration: false,
+      module: 'CommonJS',
+      emitDecoratorMetadata: true,
+      experimentalDecorators: true,
+      allowSyntheticDefaultImports: true,
+      target: 'es2017',
+      outDir: './dist',
+      baseUrl: './',
+      rootDir: './',
+      skipLibCheck: true,
+      strictNullChecks: true,
+      noImplicitAny: false,
+      forceConsistentCasingInFileNames: false,
+      noFallthroughCasesInSwitch: false,
+      noUnusedLocals: false,
+      noUnusedParameters: false,
+      paths: {
+        '@/*': ['src/*'],
+        '@lib/*': ['lib/*'],
+      },
+    },
+    exclude: ['node_modules'],
+  },
+  // Node Project Options
+  npmignoreEnabled: false,
+  buildWorkflow: false,
+  jest: false,
+  defaultReleaseBranch: constants.branches.main,
+  release: false,
+  // @ToDo 나중에 재설정
+  depsUpgrade: false,
+  depsUpgradeOptions: {
+    workflowOptions: {
+      schedule: javascript.UpgradeDependenciesSchedule.WEEKLY,
+      projenCredentials: constants.projenCredentials.githubTokenCredential,
+      assignees: [constants.author.name],
+      branches: [constants.branches.develop],
+    },
+    pullRequestTitle: 'Upgrade Node Deps',
+  },
+  prettier: true,
+  prettierOptions: {
+    settings: {
+      semi: true,
+      arrowParens: ArrowParens.AVOID,
+      endOfLine: javascript.EndOfLine.AUTO,
+      singleQuote: true,
+      tabWidth: 2,
+      trailingComma: javascript.TrailingComma.ALL,
+    },
+  },
+  // GitHub Project Options
+  githubOptions: {
+    pullRequestLintOptions: {
+      semanticTitleOptions: {
+        types: ['test', 'feat', 'fix', 'chore', 'dev'],
+      },
+    },
+  },
+
+  projenCredentials: constants.projenCredentials.githubTokenCredential,
+  authorName: constants.author.name,
+  authorEmail: constants.author.email,
+  name: constants.project.name,
+  gitignore: [
+    '.DS_STORE',
+    `/${constants.dirs.etc.cdktfOutFilePath}`,
+    constants.dirs.etc.generatedSrcDir,
+    `/${constants.dirs.etc.envDir}`,
+  ],
+  // @ToDo 이 부분 나중에 수정
+  // deps: ['flat@5.0.2', '@hapi/joi', 'joi-extract-type'],
+  // devDeps: ['flatley', '@types/flat', 'deepmerge', 'husky@8'],
 });
-project.synth();
+
+void (async () => {
+  project.synth();
+})();
