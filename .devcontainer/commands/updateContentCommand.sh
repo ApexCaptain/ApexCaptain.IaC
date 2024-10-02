f28187262dbcb4968d030647b341988542c5c371
SangHun Lee <ayteneve93@gmail.com>
2024-10-02 18:04:37 +0900

    chore: project created with projen

---
+++ .devcontainer/commands/updateContentCommand.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+echo Changing owner of paths that mounted by named volumes
+sudo chown -R $USER:$USER $volumePathsToChangeOwner
+
+echo Updating apt package manager
+sudo apt update -y
+sudo apt upgrade -y
+
+echo Installing apt packages
+sudo apt install -y \
+    sshpass
+
+echo Installing global npm packages
+npm install -g \
+    npm@latest
