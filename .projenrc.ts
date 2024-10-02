68c3899c76c8b9b3344cae6761799b9504f6fb16
SangHun Lee <ayteneve93@gmail.com>
2024-10-02 18:05:57 +0900

    Init Project

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,12 +1,8 @@
-import { typescript } from 'projen';
-const project = new typescript.TypeScriptAppProject({
-  defaultReleaseBranch: 'main',
-  name: 'ApexCaptain.IaC',
-  projenrcTs: true,
+import { typescript } from "projen";
 
-  // deps: [],                /* Runtime dependencies of this module. */
-  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],             /* Build dependencies for this module. */
-  // packageName: undefined,  /* The "name" in package.json. */
+const project = new typescript.TypeScriptAppProject({
+  defaultReleaseBranch: "main",
+  name: "ApexCaptain.IaC",
+  projenrcTs: true,
 });
 project.synth();
