dc7efb4b9821db5f51f2d38b9ff8154e34b96170
SangHun Lee <ayteneve93@gmail.com>
2024-12-08 14:30:02 +0900

    RegCommit 2024-12-08

--- src/terraform/stacks/k8s/workstation/apps/sftp.stack.ts
+++ src/terraform/stacks/k8s/workstation/apps/sftp.stack.ts
@@ -38,178 +38,176 @@ export class K8S_Workstation_Apps_Sftp_Stack extends AbstractStack {
     },
   };
 
-  // meta = (() => {
-  //   const dataDirName = 'data';
-  //   return {
-  //     name: 'sftp',
-  //     labels: {
-  //       app: 'sftp',
-  //     },
-  //     port: {
-  //       sftp: {
-  //         containerPort: 22,
-  //         servicePort: 22,
-  //         nodePort: 30001,
-  //       },
-  //     },
-  //     volume: {
-  //       sftp: {
-  //         dataDirName,
-  //         containerDirPath: `/home/${this.config.userName}/${dataDirName}`,
-  //         hostDirPath: path.join(
-  //           this.globalConfigService.config.terraform.stacks.k8s.workstation
-  //             .common.volumeDirPath.hddVolume,
-  //           'sftp-data',
-  //         ),
-  //         volumeName: 'sftp-data',
-  //       },
-  //     },
-  //   };
-  // })();
+  meta = (() => {
+    const dataDirName = 'data';
+    return {
+      name: 'sftp',
+      labels: {
+        app: 'sftp',
+      },
+      port: {
+        sftp: {
+          containerPort: 22,
+          servicePort: 22,
+          nodePort: 30001,
+        },
+      },
+      volume: {
+        sftp: {
+          dataDirName,
+          containerDirPath: `/home/${this.config.userName}/${dataDirName}`,
+          hostDirPath: path.join(
+            this.globalConfigService.config.terraform.stacks.k8s.workstation
+              .common.volumeDirPath.hddVolume,
+            'sftp-data',
+          ),
+          volumeName: 'sftp-data',
+        },
+      },
+    };
+  })();
 
-  // namespace = this.provide(Namespace, 'namespace', () => ({
-  //   metadata: {
-  //     name: this.meta.name,
-  //   },
-  // }));
+  namespace = this.provide(Namespace, 'namespace', () => ({
+    metadata: {
+      name: this.meta.name,
+    },
+  }));
 
-  // privateKey = this.provide(Resource, 'privateKey', idPrefix => {
-  //   const key = this.provide(PrivateKey, `${idPrefix}-key`, () => ({
-  //     algorithm: 'RSA',
-  //     rsaBits: 4096,
-  //   }));
+  privateKey = this.provide(Resource, 'privateKey', idPrefix => {
+    const key = this.provide(PrivateKey, `${idPrefix}-key`, () => ({
+      algorithm: 'RSA',
+      rsaBits: 4096,
+    }));
 
-  //   const privateSshKeyFileInSecrets = this.provide(
-  //     File,
-  //     `${idPrefix}-privateSshKeyFileInSecrets`,
-  //     id => ({
-  //       filename: path.join(
-  //         process.cwd(),
-  //         this.globalConfigService.config.terraform.stacks.common
-  //           .generatedKeyFilesDirRelativePaths.secrets,
-  //         `${K8S_Workstation_Apps_Sftp_Stack.name}-${id}.key`,
-  //       ),
-  //       content: key.element.privateKeyOpenssh,
-  //     }),
-  //   );
+    const privateSshKeyFileInSecrets = this.provide(
+      File,
+      `${idPrefix}-privateSshKeyFileInSecrets`,
+      id => ({
+        filename: path.join(
+          process.cwd(),
+          this.globalConfigService.config.terraform.stacks.common
+            .generatedKeyFilesDirRelativePaths.secrets,
+          `${K8S_Workstation_Apps_Sftp_Stack.name}-${id}.key`,
+        ),
+        content: key.element.privateKeyOpenssh,
+      }),
+    );
 
-  //   const privateSshKeyFileInKeys = this.provide(
-  //     File,
-  //     `${idPrefix}-privateSshKeyFileInKeys`,
-  //     id => ({
-  //       filename: path.join(
-  //         process.cwd(),
-  //         this.globalConfigService.config.terraform.stacks.common
-  //           .generatedKeyFilesDirRelativePaths.keys,
-  //         `${K8S_Workstation_Apps_Sftp_Stack.name}-${id}.key`,
-  //       ),
-  //       content: key.element.privateKeyOpenssh,
-  //       filePermission: '0600',
-  //     }),
-  //   );
+    const privateSshKeyFileInKeys = this.provide(
+      File,
+      `${idPrefix}-privateSshKeyFileInKeys`,
+      id => ({
+        filename: path.join(
+          process.cwd(),
+          this.globalConfigService.config.terraform.stacks.common
+            .generatedKeyFilesDirRelativePaths.keys,
+          `${K8S_Workstation_Apps_Sftp_Stack.name}-${id}.key`,
+        ),
+        content: key.element.privateKeyOpenssh,
+        filePermission: '0600',
+      }),
+    );
 
-  //   return [
-  //     {},
-  //     {
-  //       key,
-  //       privateSshKeyFileInSecrets,
-  //       privateSshKeyFileInKeys,
-  //     },
-  //   ];
-  // });
+    return [
+      {},
+      {
+        key,
+        privateSshKeyFileInSecrets,
+        privateSshKeyFileInKeys,
+      },
+    ];
+  });
 
-  // configMap = this.provide(ConfigMap, 'configMap', () => ({
-  //   metadata: {
-  //     name: this.meta.name,
-  //     namespace: this.namespace.element.metadata.name,
-  //   },
-  //   data: {
-  //     'ssh-public-key': this.privateKey.shared.key.element.publicKeyOpenssh,
-  //   },
-  // }));
+  configMap = this.provide(ConfigMap, 'configMap', () => ({
+    metadata: {
+      name: this.meta.name,
+      namespace: this.namespace.element.metadata.name,
+    },
+    data: {
+      'ssh-public-key': this.privateKey.shared.key.element.publicKeyOpenssh,
+    },
+  }));
 
-  // deployment = this.provide(Deployment, 'deployment', () => ({
-  //   metadata: {
-  //     name: this.meta.name,
-  //     namespace: this.namespace.element.metadata.name,
-  //   },
-  //   spec: {
-  //     replicas: '1',
-  //     selector: {
-  //       matchLabels: this.meta.labels,
-  //     },
-  //     template: {
-  //       metadata: {
-  //         labels: this.meta.labels,
-  //       },
-  //       spec: {
-  //         container: [
-  //           {
-  //             name: 'sftp',
-  //             image: 'atmoz/sftp',
-  //             args: [
-  //               `${this.config.userName}::1001:100:${this.meta.volume.sftp.dataDirName}`,
-  //             ],
-  //             port: [
-  //               {
-  //                 containerPort: this.meta.port.sftp.containerPort,
-  //               },
-  //             ],
-  //             volumeMount: [
-  //               {
-  //                 mountPath: `/home/${this.config.userName}/.ssh/keys`,
-  //                 name: this.configMap.element.metadata.name,
-  //                 readOnly: true,
-  //               },
-  //               {
-  //                 mountPath: this.meta.volume.sftp.containerDirPath,
-  //                 name: this.meta.volume.sftp.volumeName,
-  //               },
-  //             ],
-  //             securityContext: {
-  //               runAsUser: '1001',
-  //               runAsGroup: '100',
-  //               capabilities: {
-  //                 add: ['SYS_ADMIN'],
-  //               },
-  //             },
-  //           },
-  //         ],
-  //         volume: [
-  //           {
-  //             name: this.configMap.element.metadata.name,
-  //             configMap: {
-  //               name: this.configMap.element.metadata.name,
-  //             },
-  //           },
-  //           {
-  //             name: this.meta.volume.sftp.volumeName,
-  //             hostPath: {
-  //               type: 'DirectoryOrCreate',
-  //               path: this.meta.volume.sftp.hostDirPath,
-  //             },
-  //           },
-  //         ],
-  //       },
-  //     },
-  //   },
-  // }));
+  deployment = this.provide(Deployment, 'deployment', () => ({
+    metadata: {
+      name: this.meta.name,
+      namespace: this.namespace.element.metadata.name,
+    },
+    spec: {
+      replicas: '1',
+      selector: {
+        matchLabels: this.meta.labels,
+      },
+      template: {
+        metadata: {
+          labels: this.meta.labels,
+        },
+        spec: {
+          container: [
+            {
+              name: 'sftp',
+              image: 'atmoz/sftp',
+              args: [
+                `${this.config.userName}::1001:100:${this.meta.volume.sftp.dataDirName}`,
+              ],
+              port: [
+                {
+                  containerPort: this.meta.port.sftp.containerPort,
+                },
+              ],
+              volumeMount: [
+                {
+                  mountPath: `/home/${this.config.userName}/.ssh/keys`,
+                  name: this.configMap.element.metadata.name,
+                  readOnly: true,
+                },
+                {
+                  mountPath: this.meta.volume.sftp.containerDirPath,
+                  name: this.meta.volume.sftp.volumeName,
+                },
+              ],
+              securityContext: {
+                capabilities: {
+                  add: ['SYS_ADMIN'],
+                },
+              },
+            },
+          ],
+          volume: [
+            {
+              name: this.configMap.element.metadata.name,
+              configMap: {
+                name: this.configMap.element.metadata.name,
+              },
+            },
+            {
+              name: this.meta.volume.sftp.volumeName,
+              hostPath: {
+                type: 'DirectoryOrCreate',
+                path: this.meta.volume.sftp.hostDirPath,
+              },
+            },
+          ],
+        },
+      },
+    },
+  }));
 
-  // service = this.provide(Service, 'service', () => ({
-  //   metadata: {
-  //     name: this.meta.name,
-  //     namespace: this.namespace.element.metadata.name,
-  //   },
-  //   spec: {
-  //     type: 'NodePort',
-  //     selector: this.meta.labels,
-  //     port: Object.values(this.meta.port).map(port => ({
-  //       port: port.servicePort,
-  //       targetPort: port.containerPort.toString(),
-  //       nodePort: port.nodePort,
-  //     })),
-  //   },
-  // }));
+  service = this.provide(Service, 'service', () => ({
+    metadata: {
+      name: this.meta.name,
+      namespace: this.namespace.element.metadata.name,
+    },
+    spec: {
+      type: 'NodePort',
+      selector: this.meta.labels,
+      port: Object.values(this.meta.port).map(port => ({
+        port: port.servicePort,
+        targetPort: port.containerPort.toString(),
+        nodePort: port.nodePort,
+      })),
+    },
+  }));
 
   constructor(
     // Global
